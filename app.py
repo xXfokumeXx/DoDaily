527222320342fbbe6e4c8239381eed9baafd1d85
xXfokumeXx <radek.cerny847@gmail.com>
2025-07-14 23:31:07 +0200

    updated app.py

--- app.py
+++ app.py
@@ -1,12 +1,18 @@
 
-from flask import Flask, render_template, request
+from flask import Flask, render_template
 
 
 def create_app():
     app = Flask(__name__)
 
-    @app.route("/", methods=["GET", "POST"])
-    def home():
-        return render_template("base.html")
+
+    @app.route("/")
+    def index():
+        return render_template("index.html", title="Do Daily - Home")
+    
+
+    @app.route("/add", methods=["GET", "POST"])
+    def add_habit():
+        return render_template("add_habbit.html", title="Do Daily - Add Habit")
     
     return app
