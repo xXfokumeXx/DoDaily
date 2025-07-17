545d9a781a1ade87780023a6682d799256dbca37
xXfokumeXx <radek.cerny847@gmail.com>
2025-07-17 23:02:40 +0200

    hardcoded task print mechanism, repaired name of the method for add_task

--- app.py
+++ app.py
@@ -1,18 +1,21 @@
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 
 def create_app():
     app = Flask(__name__)
+    habits = ["Test Task"]
 
 
     @app.route("/")
     def index():
-        return render_template("index.html", title="Do Daily - Home")
+        return render_template("index.html", habits=habits, title="Do Daily - Home")
     
 
     @app.route("/add", methods=["GET", "POST"])
-    def add_habit():
+    def add_task():
+        if request.method == "POST":
+            pass
         return render_template("add_task.html", title="Do Daily - Add Habit")
     
     return app
