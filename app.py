63e0ffe79c92d685a43bee2c90bf845fd940ae4f
xXfokumeXx <radek.cerny847@gmail.com>
2025-07-18 22:35:51 +0200

    added a mechanism for choosing a date for assigning tasks

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-
+import datetime
 from flask import Flask, render_template, request
 
 
@@ -6,10 +6,26 @@ def create_app():
     app = Flask(__name__)
     habits = ["Test Task"]
 
+    def date_range(start: datetime.date):
+        dates = [start + datetime.timedelta(days=diff) for diff in range(-3,4)]
+        return dates
+
 
     @app.route("/")
     def index():
-        return render_template("index.html", habits=habits, title="Do Daily - Home")
+        date_str = request.args.get("date")
+        if date_str:
+            selected_date = datetime.date.fromisoformat(date_str)
+        else:
+            selected_date = datetime.date.today()
+        return render_template(
+            "index.html",
+            habits=habits,
+            title="Do Daily - Home",
+            date_range=date_range,
+            selected_date=selected_date
+        )
+
     
 
     @app.route("/add", methods=["GET", "POST"])
